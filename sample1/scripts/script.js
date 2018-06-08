bd90425185ac5a05b410380d70bb8eaf4e6b6a75
Wojtek <wojtek@Wojteks-MacBook-Air.local>
2018-06-08 21:27:31 +0100

    Script

--- sample1/scripts/script.js
+++ sample1/scripts/script.js
@@ -1,11 +1,29 @@
-//
 // Available modules include (this is not a complete list):
-// var Scene = require('Scene');
-// var Textures = require('Textures');
-// var Materials = require('Materials');
-// var FaceTracking = require('FaceTracking');
-// var Animation = require('Animation');
-// var Reactive = require('Reactive');
+const Scene = require('Scene');
+const Textures = require('Textures');
+const Materials = require('Materials');
+const FaceTracking = require('FaceTracking');
+const Animation = require('Animation');
+const Reactive = require('Reactive');
+const Diagnostics = require('Diagnostics');
+const Time = require('Time');
+
+const face0 = FaceTracking.face(0);
+const face1 = FaceTracking.face(1);
+const mouth0 = face0.cameraTransform.applyTo(face0.mouth.center);
+const mouth1 = face1.cameraTransform.applyTo(face1.mouth.center);
+
+const mouthXDist = mouth0.x.sub(mouth1.x);
+const mouthYDist = mouth0.y.sub(mouth1.y);
+const mouthZDist = mouth0.z.sub(mouth1.z);
+const distance = mouthXDist.pow(2).add(mouthYDist.pow(2)).add(mouthZDist.pow(2)).pow(0.5);
+
+Time.ms.monitor().subscribe(function () {
+Diagnostics.log( + distance.lastValue);
+});
+
+
+
 //
 // Example script
 //
